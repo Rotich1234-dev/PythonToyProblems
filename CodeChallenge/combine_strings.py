ee85861e73b49a7ff79d1142f595fabb88a1ad54
Rotich <jane.rotich@student.moringaschool.com>
2024-03-03 15:40:46 +0300

    feat:codewar-2

--- CodeChallenge/combine_strings.py
+++
@@ -1,3 +0,0 @@
-def combine_names(a, b):
-    return a + " " + b
-print (combine_names(2, 14))
---
+++ Codewars/combine_strings.py
@@ -0,0 +1,4 @@
+def combine_names(a, b):
+    return str(a) + " " + str(b)
+print (combine_names('Jane', 'Rotich'))
+print (combine_names('Moringa', 'School'))
