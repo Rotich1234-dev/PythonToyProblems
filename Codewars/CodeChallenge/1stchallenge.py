050b5addbe7616e226b2fae304223202c91b490f
Rotich <jane.rotich@student.moringaschool.com>
2024-03-14 11:08:36 +0300

    feat:fix challenge1

--- Codewars/CodeChallenge/1stchallenge.py
+++ Codewars/CodeChallenge/1stchallenge.py
@@ -1,15 +1,15 @@
 def solving(A):
-# getting number of boxes
+    # getting number of boxes
     N = len(A)
     #  target number of bricks in each box
     min_bricks = 10
-     # Initialize the total number of moves
+    # Initialize the total number of moves
     total_moves = 0
     # iterate through the boxes
     for i in range(N - 1, 0, -1):
-         # Calculate the difference between the bricks in the current box and the target
+        # Calculate the difference between the bricks in the current box and the target
         deficit = A[i] - min_bricks
-         # Add the absolute value of the difference to the total moves
+        # Add the absolute value of the difference to the total moves
         total_moves += abs(deficit)
         # Update the number of bricks in the box to the target
         A[i - 1] += deficit
@@ -18,6 +18,8 @@ def solving(A):
         return total_moves
     else:
         return -1
-# test case
-print(solving([11, 10, 8, 12, 8, 10, 11]))  
-print(solving([7, 14, 10])) 
+
+# test case 
+print(solving([7, 15, 10, 8]))  
+print(solving([7, 14, 10]))
+print(solving([11, 10, 8, 12, 8, 10, 11]))
